2f5cf547f478b4b4fee782a5085f6ef1558e1e8e
nsdsilva <natalisamaraa@gmail.com>
2021-06-17 20:09:22 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/natsamara/cursomc/domain/Endereco.java
+++ src/main/java/com/natsamara/cursomc/domain/Endereco.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.core.sym.Name;
 
 @Entity
@@ -25,11 +26,12 @@ public class Endereco implements Serializable {
 	private String bairro;
 	private String cep;
 	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
-	
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name = "cidade_id")
 	private Cidade cidade;
